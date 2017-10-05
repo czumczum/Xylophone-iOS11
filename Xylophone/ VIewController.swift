1e16bce4af049261ee9cd7c8642663580946795a
Ula <slodkoscigit config --global user.email slodkosci@gmail.comgit config --global user.email slodkosci>
2017-10-05 13:14:05 -0400

    Xylophone linked with sounds

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
+        var numberOfNote = 1
+        switch sender.tag {
+        case 2:
+            numberOfNote = 2
+        case 3:
+            numberOfNote = 3
+        case 4:
+            numberOfNote = 4
+        case 5:
+            numberOfNote = 5
+        case 6:
+            numberOfNote = 6
+        case 7:
+            numberOfNote = 7
+        default:
+            numberOfNote = 1
+        }
+        let url = Bundle.main.url(forResource: "note\(numberOfNote)", withExtension: "wav")
         
-        
-        
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+            guard let player = player else {return}
+            
+            player.prepareToPlay()
+            player.play()
+        }
+        catch let error as Error {
+            print(error.localizedDescription)
+        }
+
     }
-    
-  
 
 }
 
