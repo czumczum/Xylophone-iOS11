abf8dc4f53d452dbd65b2923a9784163ddf7000c
Ula <slodkosci@gmail.com>
2017-10-09 14:09:19 -0400

    Some refactor

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,17 +1,8 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController{
     
-
     var player: AVAudioPlayer?
     
     override func viewDidLoad() {
@@ -19,24 +10,11 @@ class ViewController: UIViewController{
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        var numberOfNote = 1
-        switch sender.tag {
-        case 2:
-            numberOfNote = 2
-        case 3:
-            numberOfNote = 3
-        case 4:
-            numberOfNote = 4
-        case 5:
-            numberOfNote = 5
-        case 6:
-            numberOfNote = 6
-        case 7:
-            numberOfNote = 7
-        default:
-            numberOfNote = 1
-        }
-        let url = Bundle.main.url(forResource: "note\(numberOfNote)", withExtension: "wav")
+        playSound(note: sender.tag)
+    }
+
+    func playSound(note: Int) {
+        let url = Bundle.main.url(forResource: "note\(note)", withExtension: "wav")
         
         do {
             player = try AVAudioPlayer(contentsOf: url!)
@@ -45,11 +23,8 @@ class ViewController: UIViewController{
             player.prepareToPlay()
             player.play()
         }
-        catch let error as Error {
-            print(error.localizedDescription)
+        catch let error as NSError {
+            print(error)
         }
-
     }
-
 }
-
